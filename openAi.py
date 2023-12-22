3e0c8f4207b4ac27cd9aaad53b47187577bb6e2c
Martin Jørgensen <127878026+Martiwj@users.noreply.github.com>
2023-12-22 15:05:03 +0100

    Update openAi.py

--- openAi.py
+++ openAi.py
@@ -27,3 +27,17 @@ def get_chat_completion(prompt, model="gpt-4"):
     # Returning the extracted response
     return generated_content
 
+
+def create_image(prompt):
+    response = client.images.generate(
+    model="dall-e-3",
+    prompt=prompt, 
+    size="1024x1024",
+    quality="standard",
+    n=1,
+    )   
+
+    image_url = response.data[0].url
+    return image_url
+
+
